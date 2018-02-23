c7b42d1383f4887e879521f491722f9cf7ea031f
sinclairstudy <sinclairstudy@gmail.com>
2018-02-23 14:48:50 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+require_relative 'User'
 
+class Student < User
+
+  def initialize
+    @knowledge = []
+  end
+
+  attr_reader :knowledge
+
+  def learn(string)
+    knowledge << string
+  end
 end
